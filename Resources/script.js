6a478936ff18fb2152f6fe28b30b59eaf596ce79
Alex Marzullo <marzullo94@gmail.com>
2020-11-14 22:27:00 -0500

    Added comments and updating files

--- Resources/script.js
+++ Resources/script.js
@@ -6,10 +6,10 @@ var passwordText = document.querySelector("#password")
 // Function to Generate Code
 function writePassword() {
     
-  //Prompt to ask for password Options
-  var pasLength = parseInt(prompt("How many characters would you like your password to be? (8-128)"))
+    //Prompt to ask for password Options
+    var pasLength = parseInt(prompt("How many characters would you like your password to be? (8-128)"))
   
-  if((pasLength >= 8) && (pasLength <= 128)) {
+    if((pasLength >= 8) && (pasLength <= 128)) {
     
     //Defining the variables used in password
     var numbers = "0123456789";
@@ -38,21 +38,21 @@ function writePassword() {
           passPool.push(symbols[i])
         }
       }
-
-      
+     
       if(lowConfirm) {
         for(i = 0; i < lowerCase.length; ++i) {
           passPool.push(lowerCase[i])
         }
       }
-
-      
+     
       if(uppConfirm) {
         for(i = 0; i < upperCase.length; ++i) {
           passPool.push(upperCase[i])
         }
       }
-
+      
+      console.log(passPool)
+     
       //After the condition statements are pushed, the code is randomized with the following statement.
       let passDraft = []; {
         for(let i = 0; i < pasLength; ++i) {
@@ -62,14 +62,14 @@ function writePassword() {
       }
         
       console.log(passDraft)
-        
-      
+              
       var finalPass = passDraft.join('')
-
-      
+     
       document.getElementById("password").textContent = finalPass
+  }else {
+    var incorrectPw = alert("Password has to be between 8-128 characters")
   }
-}
+} 
 
 
 
